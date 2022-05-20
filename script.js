a7b3d02710ab95897c69bb02b2bce4c4fbc89310
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-20 14:20:15 -0400

    Added functionality to create custom grids

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Create 16x16 grid of square divs
-function createGrid() {
+// Create a grid, but default to a 16 by 16 grid
+function createGrid(rows = 16, columns = 16) {
     const container = document.querySelector('#container');
-    for (let row = 0; row < 16; row++) { // Make 16 rows
+    const grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+    for (let row = 0; row < rows; row++) { // Make 16 rows
         const row = document.createElement('div');
         row.setAttribute('id', 'row');
-        for (let column = 0; column < 16; column++) { // For every row, make 16 columns
+        for (let column = 0; column < columns; column++) { // For every row, make 16 columns
             const square = document.createElement('div');
             square.setAttribute('id', 'square');
             row.appendChild(square);
         };
-        container.appendChild(row);
+        grid.appendChild(row);
     };
+    container.appendChild(grid);
+    setupEvents();
 }
 
 // Set up the event listeners for every square
@@ -23,8 +27,22 @@ function setupEvents() {
     });
 }
 
-// Calls both other functions to setup the grid, and configure the event listeners for it
-function setupGrid() { 
-    createGrid();
-    setupEvents();
+// Set up the event listener for the custom grid button
+const customButton = document.querySelector('#custom')
+customButton.addEventListener('click', () => {
+    const userColumns = prompt("How wide would you like the grid to be?");
+    const userRows = prompt("How high would you like the grid to be?");
+    clearContainer();
+    createGrid(userRows, userColumns);
+});
+
+// Function to clear all grids from container
+function clearContainer() {
+    const container = document.querySelector('#container');
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
+
+// Load basic grid on page load
+createGrid();
